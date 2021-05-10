c2e1e5fe062c6174a63998b651bd24836b4525b3
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-05-10 15:53:09 -0700

    Reorganized Code Into Different Files
    Separated main, note, post, form, header, and footer php files into different files and connected via main.

---
+++ main.php
@@ -0,0 +1,62 @@
+<main class="flex-1" style="-webkit-overflow-scrolling:touch">
+    <div class="container bg-white mx-auto border">
+            <div class="text-center font-bold text-xl p-2">
+            Settings
+            <button class="hover:bg-blue-500 focus:outline-none bg-blue-100 border-blue-400 text-blue-400 transition rounded hover:text-white font-bold ml-2 p-2"> Confirm </button>  
+            </div>
+            <div class="flex flex-col items-center  ">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-300">
+            <input placeholder="Name" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+            </div>
+            </div>
+
+            <div class="flex flex-col items-center pt-5">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
+            <input placeholder="@Username" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+            </div>
+            </div>
+            <div class="flex flex-col items-center pt-5">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
+            <input placeholder="Password" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+            </div>
+            </div>
+            <div class="flex flex-col items-center pt-5">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
+            <input placeholder="Confirm Password" class="placeholder-red-400 focus:outline-none rounded p-2"/>
+            </div>
+            </div>
+            <div class="flex flex-col items-center pt-5">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
+            <input placeholder="Email" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+            </div>
+            </div>
+            <div class="flex flex-col items-center pt-5">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
+            <input placeholder="Confirm Email" class="placeholder-red-400 focus:outline-none rounded p-2"/>
+            </div>
+            </div>
+            <div class="flex flex-col items-center pt-5">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
+            <input placeholder="Bio" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+            </div>
+            <div class="flex flex-col items-center pt-6 pb-2 pr-14 text-blue-600">Notification Settings</div>
+            <div class="flex flex-col items-center pt-2 pr-2">
+            <label class="inline-flex">
+                <input type="checkbox" class="form-checkbox h-5 w-5 text-red-800 m-2" checked><span class="m-2 text-gray-700">Only mentions active </span>
+            </label>
+            </div>
+            <div class="flex flex-col items-center pt-1">
+            <label class="inline-flex">
+                <input type="checkbox" class="form-checkbox h-5 w-5 text-red-800 m-2" checked><span class="m-2 text-gray-700">All notifications active</span>
+            </label>
+            <div class = "flex flex-row items-center p-5">		
+            <button class="hover:bg-green-400 bg-green-500 border-green-200 transition text-white font-semibold hover:text-white font-semibold p-1">
+                    Steam
+                    </button>
+            <button class="hover:bg-blue-500 focus:outline-none bg-blue-100 border-blue-400 text-blue-400 transition rounded hover:text-white font-bold ml-16 p-2"> Confirm </button>         
+                    </div>
+            </div>
+</div>
+    </div>						
+    </div>
+</main>
