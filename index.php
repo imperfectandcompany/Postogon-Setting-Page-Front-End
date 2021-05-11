c75c48f13100499c5ad3f723e41a9d6f5e0a6b61
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-05-10 18:31:53 -0700

    Redesigned Settings Page Updates
    Changed the format of the settings page to be within multiple tabs and use modal like aspects

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8" />
+  <title>Postogon Settings Page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
@@ -12,6 +13,18 @@
 <?php include_once('header.php'); ?>
 <!-- main -->
 <?php include_once('main.php'); ?>
+
+<!-- 
+Change username,
+Change profile picture,
+Change Username,
+Change e-mail,
+Change password,
+Change Bio,
+Link steam,
+Log out devices,
+Manage followers & contacts
+-->
 <!-- footer -->
 <?php include_once('footer.php'); ?>
 </body>
--- main.php
+++ main.php
@@ -1,59 +1,34 @@
 <main class="flex-1" style="-webkit-overflow-scrolling:touch">
-    <div class="container bg-white mx-auto border">
-            <div class="text-center font-bold text-xl p-2">
-            Settings
-            <button class="hover:bg-blue-500 focus:outline-none bg-blue-100 border-blue-400 text-blue-400 transition rounded hover:text-white font-bold ml-2 p-2"> Confirm </button>  
-            </div>
-            <div class="flex flex-col items-center  ">
-            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-300">
-            <input placeholder="Name" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
-            </div>
-            </div>
+ 
 
-            <div class="flex flex-col items-center pt-5">
-            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
-            <input placeholder="@Username" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+
+
+<div class="container bg-white mx-auto border">
+            <div class="text-center font-bold text-xl p-2 pr-36">
+            Profile
+            <!-- <button class="hover:bg-blue-500 focus:outline-none bg-blue-100 border-blue-400 text-blue-400 transition rounded hover:text-white font-bold ml-2 p-2"> Confirm </button>   -->
+            </div>
+            <div class="flex flex-col items-center pt-3 ">
+            <div>
+            <input placeholder="First Name" class="border placeholder-grey-400 focus:outline-none rounded p-3 w-28"/> 
+            <input placeholder="Last Name" class="border placeholder-grey-400 focus:outline-none rounded p-3 w-28"/> 
             </div>
             </div>
-            <div class="flex flex-col items-center pt-5">
-            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
-            <input placeholder="Password" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
-            </div>
-            </div>
-            <div class="flex flex-col items-center pt-5">
-            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
-            <input placeholder="Confirm Password" class="placeholder-red-400 focus:outline-none rounded p-2"/>
-            </div>
-            </div>
-            <div class="flex flex-col items-center pt-5">
-            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
-            <input placeholder="Email" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
-            </div>
+            <div class="flex flex-col items-center pt-5 ">
+            <div class="border rounded focus:ring-blue-700  transition hover:border-blue-300">
+            <input placeholder="Last Name" class="placeholder-grey-400 focus:outline-none rounded p-2 w-60"/> 
             </div>
             <div class="flex flex-col items-center pt-5">
             <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
-            <input placeholder="Confirm Email" class="placeholder-red-400 focus:outline-none rounded p-2"/>
+            <input placeholder="@Username" class="placeholder-grey-400 focus:outline-none rounded p-2 w-60"/>
             </div>
             </div>
-            <div class="flex flex-col items-center pt-5">
+            <div class="flex flex-col items-center pt-5 h-32 w- 64">
             <div class="border rounded focus:ring-blue-700  transition hover:border-blue-700">
-            <input placeholder="Bio" class="placeholder-grey-400 focus:outline-none rounded p-2"/>
+            <input placeholder="Bio" class="placeholder-grey-400 focus:outline-none rounded p-2 h-24 w-60">
             </div>
-            <div class="flex flex-col items-center pt-6 pb-2 pr-14 text-blue-600">Notification Settings</div>
-            <div class="flex flex-col items-center pt-2 pr-2">
-            <label class="inline-flex">
-                <input type="checkbox" class="form-checkbox h-5 w-5 text-red-800 m-2" checked><span class="m-2 text-gray-700">Only mentions active </span>
-            </label>
-            </div>
-            <div class="flex flex-col items-center pt-1">
-            <label class="inline-flex">
-                <input type="checkbox" class="form-checkbox h-5 w-5 text-red-800 m-2" checked><span class="m-2 text-gray-700">All notifications active</span>
-            </label>
             <div class = "flex flex-row items-center p-5">		
-            <button class="hover:bg-green-400 bg-green-500 border-green-200 transition text-white font-semibold hover:text-white font-semibold p-1">
-                    Steam
-                    </button>
-            <button class="hover:bg-blue-500 focus:outline-none bg-blue-100 border-blue-400 text-blue-400 transition rounded hover:text-white font-bold ml-16 p-2"> Confirm </button>         
+            <button class="hover:bg-blue-500 focus:outline-none bg-blue-100 border-blue-400 text-blue-400 transition rounded hover:text-white font-bold ml-32 p-2"> Confirm </button>         
                     </div>
             </div>
 </div>
